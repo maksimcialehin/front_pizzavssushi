6be4ffc6da0cb9c5da00c4181b51b3ca72fb2c91
maksimcialehin <madqfrog1863@gmail.com>
2022-11-30 22:53:48 +0300

    adding axios

--- src/pizzerias/pizzerialist.js
+++ src/pizzerias/pizzerialist.js
@@ -1,13 +1,26 @@
 import React, { Component } from 'react';
-import Data from '../data.json';
+import Data from './data.json';
+import PizzaDetail from './pizzeriadetail'
+import axios from 'axios';
 
 class PizzaList extends Component {
+    componentDidMount(){
+        axios.get("http://127.0.0.1:8000/")
+        .then((response) => {
+            console.log(response)
+        })
+        .catch(function (error) {
+            console.log(error);
+            })
+        }
+    
     render(){
         return(
             <div>
-                {Data.map( p =>
-                    <h4>{p.restaurant_name} - {p.city}</h4>
-                    )            
+                {Data.map( item =>{
+                return <PizzaDetail p = {item}/>
+
+                    })            
                 }
             </div>
         )
