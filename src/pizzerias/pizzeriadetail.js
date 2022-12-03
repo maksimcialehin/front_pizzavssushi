42a0847fe90504688185a89781a2f1c29dd548da
maksimcialehin <madqfrog1863@gmail.com>
2022-12-03 22:50:49 +0300

    adding Update view

--- src/pizzerias/pizzeriadetail.js
+++ src/pizzerias/pizzeriadetail.js
@@ -1,6 +1,19 @@
 import React, { Component } from 'react';
+import PizzaUpdate from "./pizzeriaupdate"
 
 class PizzaDetail extends Component{
+    constructor(props) {
+        super(props);
+        this.state = {
+            showComponent: false,
+        };
+        this.updatePizzeriaDetails = this.updatePizzeriaDetails.bind(this);
+    }
+
+    updatePizzeriaDetails() {
+        this.setState({ showComponent: true });
+    }
+
     render(){
         const obj = this.props.pizzeriaDetail;
         return(
@@ -11,6 +24,11 @@ class PizzaDetail extends Component{
                 </h5>
                 <h6>{obj.phone_number}</h6>
                 <p>{obj.description}</p>
+                <button style={{ backgroundColor: "white"}}
+                        onClick={() => this.updatePizzeriaDetails()}>
+                            Update
+                </button>
+                {this.state.showComponent ? <PizzaUpdate pizzeriaUpdate={obj} /> : null}
             </div>
         );
     }
